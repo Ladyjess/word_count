2ae091ad6fd81282a4b5623a2b92627f92ea0966
Jessica <ladyjtm@gmail.com>
2015-02-13 10:07:49 -0800

    passing specs and code'

--- spec/wordcount_spec.rb
+++ spec/wordcount_spec.rb
@@ -3,10 +3,15 @@ require 'wordcount'
 
 describe 'String#word_count' do
 
-  it 'takes a string and counts the word.' do
+  it 'takes a string and counts the word' do
     expect('hello'.word_count('hello')).to eq 1
   end
 
-  it 'takes a word that the user inputs and tells how many times the word will appear.'
+  it 'takes a word that the user inputs and tells how many times the word will appear' do
     expect('Today it was very sunny and today I am happy'.word_count('today')).to eq 2
-  end  
+  end
+
+  it 'gives the sentence a zero for empty forms' do
+    expect(''.word_count('hi')).to eq 0
+  end
+end
